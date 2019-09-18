21643ce61266289c217b448fbbce21c39035c6e5
James Curran <dzejms@gmail.com>
2019-09-17 21:43:46 -0400

    cleaned up the initial poc

--- src/server/generation/GetNewFrame/index.js
+++ src/server/generation/GetNewFrame/index.js
@@ -4,28 +4,42 @@ module.exports = async function (context, myTimer) {
     const request = require('request');
     const azure = require('azure-storage');
 
-    const imageUri = 'https://fl511.com/map/Cctv/647--12';
-    var timeStamp = new Date().toISOString();
+    const imageUri = getCameraUri();
     
     const blobService = azure.createBlobService(process.env["AzureWebJobsStorage"]);
-    blobService.createContainerIfNotExists('images', {
-    publicAccessLevel: 'blob'
-    }, function(error, result, response) {
+    blobService.createContainerIfNotExists('images', {publicAccessLevel: 'blob' }, function(error, result, response) {
         if (!error) {
-            console.log('result:' ,result);
-            // if result = true, container was created.
-            // if result = false, container already existed.
-            const stream = blobService.createWriteStreamToBlockBlob('images', 'file2.jpeg');
+            const stream = getBlobStream(blobService);
             request(imageUri, (error, response, body) => {
-                console.error('error:', error);
-                console.log('statusCode:', response && response.statusCode);
-        
-                
+                if (error) {
+                    context.error(error);
+                }
             }).pipe(stream);
         }
     });
-
     
-
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    function getCameraUri() {
+        return 'https://fl511.com/map/Cctv/647--12';
+        /*
+            Known list of Skyway camera URIs
+            https://fl511.com/map/Cctv/645--12
+            https://fl511.com/map/Cctv/647--12
+            https://fl511.com/map/Cctv/652--12
+            https://fl511.com/map/Cctv/654--12
+            https://fl511.com/map/Cctv/710--12
+            https://fl511.com/map/Cctv/724--12
+            https://fl511.com/map/Cctv/726--12
+            https://fl511.com/map/Cctv/813--12
+        */
+    }
+        
+    function getBlobStream(blobService) {
+        const fileName = new Date().getTime().toString();
+        return blobService.createWriteStreamToBlockBlob('images', fileName);
+    }
+        
+    const timeStamp = new Date().toISOString();
+    context.log('JavaScript timer trigger function ran!', timeStamp);
 };
+
+

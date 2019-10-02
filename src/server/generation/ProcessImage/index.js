6dbe8d94b7fe85afc59602a32eaebf99727a5ee6
Dzejms <dzejms@gmail.com>
2019-10-01 19:11:34 -0700

    Successfully installed and used opencv on ubunbu!

--- src/server/generation/ProcessImage/index.js
+++ src/server/generation/ProcessImage/index.js
@@ -1,3 +1,22 @@
 module.exports = async function (context, newImageQueueItem) {
     context.log('JavaScript queue trigger function processed work item', newImageQueueItem);
+
+    const cv = require('opencv4nodejs');
+    const fs = require("fs");
+
+    const rows = 100; // height
+    const cols = 100; // width
+ 
+    // empty Mat
+    const emptyMat = new cv.Mat(rows, cols, cv.CV_8UC3);
+    
+    // fill the Mat with default value
+    const whiteMat = new cv.Mat(rows, cols, cv.CV_8UC1, 255);
+    const blueMat = new cv.Mat(rows, cols, cv.CV_8UC3, [255, 0, 0]);
+
+    const homedir = require('os').homedir();
+    if (fs.existsSync(homedir)) {
+        cv.imwrite(homedir + '/img.png', blueMat);
+    }
+    
 };

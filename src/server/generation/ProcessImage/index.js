0eda8b7b643a1f99a0a165716fe9003af9e5fe50
Dzejms <dzejms@gmail.com>
2019-10-05 17:21:16 -0700

    spike of reading and writing via opencv successful!

--- src/server/generation/ProcessImage/index.js
+++ src/server/generation/ProcessImage/index.js
@@ -1,71 +1,68 @@
 module.exports = async function (context, newImageQueueItem) {
     context.log('JavaScript queue trigger function processed work item', newImageQueueItem);
-
-    const cv = require('opencv4nodejs');
-    const fs = require("fs");
-
-    const rows = 100; // height
-    const cols = 100; // width
- 
-    // empty Mat
-    const emptyMat = new cv.Mat(rows, cols, cv.CV_8UC3);
-    
-    // fill the Mat with default value
-    const whiteMat = new cv.Mat(rows, cols, cv.CV_8UC1, 255);
-    const blueMat = new cv.Mat(rows, cols, cv.CV_8UC3, [255, 0, 0]);
-
-    const homedir = require('os').homedir();
-    if (fs.existsSync(homedir)) {
-        cv.imwrite(homedir + '/img.png', blueMat);
-    }
     
     const azure = require('azure-storage');
-    const Stream = require('stream');
+    const cv = require('opencv4nodejs');
+    const os = require('os');
+    const fs = require('fs');
+    const path = require('path');
     
     const storageContainerName = 'images';
     const blobService = azure.createBlobService(process.env["AzureWebJobsStorage"]);
     const fileName = newImageQueueItem;
-    // const writableStream = new Stream.Writable()
-    // blobService.createReadStream(storageContainerName, fileName).pipe(writableStream);
+
+    const homeDir = os.homedir();
+    const workDirName = 'overflow';
+    const workDir = path.join(homeDir, workDirName);
     
-    const retryOperations = new azure.ExponentialRetryPolicyFilter();
-    const tableService = azure.createTableService(process.env["AzureWebJobsStorage"]).withFilter(retryOperations);
-    const tableName = 'testTable';
-    tableService.createTableIfNotExists(tableName, function(error, result, response){
-        if(!error){
-            const entity = getEntity(fileName);
-            tableService.insertEntity(tableName, entity, function (error, result, response) {
-                if(!error){
-                  // Entity inserted
-                }
-              });
+    blobService.getBlobToLocalFile(storageContainerName, fileName, path.join(workDir, 'temp.jpg'), (error, result) => {
+      const data = processImage();
+      storeData(data);
+    });
+
+    function processImage() {
+      const mat = cv.imread(path.join(workDir, 'temp.jpg'));
+      cv.imwrite(path.join(workDir, 'roundTrip.png'), mat);
+    }
+    
+    function storeData(data) {
+      const retryOperations = new azure.ExponentialRetryPolicyFilter();
+      const tableService = azure.createTableService(process.env["AzureWebJobsStorage"]).withFilter(retryOperations);
+      const tableName = 'testTable';
+      tableService.createTableIfNotExists(tableName, function(error, result, response){
+          if(!error){
+              const entity = getEntity(fileName);
+              tableService.insertEntity(tableName, entity, function (error, result, response) {
+                  if(!error){
+                    // Entity inserted
+                  }
+                });
+          }
+        });  
+
+        function getEntity(fileName) {
+          const generator = azure.TableUtilities.entityGenerator;
+          var entity = {
+              PartitionKey: generator.String(getPartitionKey()),
+              RowKey: generator.String(getRowKey(fileName)),
+              message: generator.String('This is a successful test!'),
+              goodDate: generator.DateTime(new Date(Date.UTC(2012, 8, 14))),
+            };
+          return entity;
         }
-      });
-
-      
-      function getEntity(fileName) {
-        const generator = azure.TableUtilities.entityGenerator;
-        var entity = {
-            PartitionKey: generator.String(getPartitionKey()),
-            RowKey: generator.String(getRowKey(fileName)),
-            message: generator.String('This is a successful test!'),
-            goodDate: generator.DateTime(new Date(Date.UTC(2012, 8, 14))),
-          };
-        return entity;
-      }
-
-      function getPartitionKey() {
-        const d = new Date();
-        const year = d.getFullYear();
-        const month = ("0"+(d.getMonth()+1)).slice(-2);
-        const day = ("0" + d.getDate()).slice(-2)
-        const partitionKey =  year + month + day;
-        return partitionKey;
-      }
-
-      function getRowKey(fileName) {
-          let rowKey = fileName.split('.jpg')[0];
-          return rowKey;
-      }
-
+  
+        function getPartitionKey() {
+          const d = new Date();
+          const year = d.getFullYear();
+          const month = ("0"+(d.getMonth()+1)).slice(-2);
+          const day = ("0" + d.getDate()).slice(-2)
+          const partitionKey =  year + month + day;
+          return partitionKey;
+        }
+  
+        function getRowKey(fileName) {
+            let rowKey = fileName.split('.jpg')[0];
+            return rowKey;
+        }
+    }
 };

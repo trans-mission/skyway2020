9b32544cee87707e040282e727052fddf912c003
James Curran <dzejms@gmail.com>
2020-03-20 11:25:43 -0400

    now getting and filtering top 6

--- src/server/api/GetLatestData/index.js
+++ src/server/api/GetLatestData/index.js
@@ -1,16 +1,31 @@
 const azure = require('azure-storage');
+const _ = require('lodash');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const retryOperations = new azure.ExponentialRetryPolicyFilter();
     const tableSvc = azure.createTableService(process.env["AzureWebJobsStorage"]).withFilter(retryOperations);
-    var query = new azure.TableQuery().where('PartitionKey eq ?', '20200319');
+
+    const getPartitionKey = () => {
+      const today = new Date();
+      const result = today.toISOString().slice(0,10).replace(/-/g,"");
+      return result;
+    }
+
+    const getFilteredSet = (todaysData) => {
+      const filteredSet = _.orderBy(todaysData, ['RowKey._'], ['desc']);
+      const result = _.take(filteredSet, 6);
+      return result;
+    }
+
+    const partitionKey = getPartitionKey();
+    var query = new azure.TableQuery()
+      .where('PartitionKey eq ?', partitionKey);
 
     tableSvc.queryEntities('testTable2',query, null, function(error, result, response) {
         if(!error) {
-          // query was successful
-          console.log(result);
+          const filteredSet = getFilteredSet(result.entries);
         }
       });
 
@@ -24,3 +39,4 @@ module.exports = async function (context, req) {
         body: responseMessage
     };
 }
+

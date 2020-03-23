a8cf319f221b8808cccdd52231af25163c57fbde
James Curran <dzejms@gmail.com>
2020-03-22 21:56:37 -0400

    Force it!

--- src/client/server.js
+++ src/client/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
-const port = process.env.port || 8889;
+const port = process.env.port || 8080;
 
 const dataService = require('./server/services/dataService');
 
